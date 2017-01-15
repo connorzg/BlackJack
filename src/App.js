e20a8e63a69e42e4554d421f9cd75d7ebe48b575
Connor Garber <connor.garber@gmail.com>
2017-01-15 16:51:23 -0600

    Fisher-Yates shuffle and renders three random cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-const cards = ['2C','QC'];
 
 class App extends Component {
   constructor() {
@@ -10,8 +9,7 @@ class App extends Component {
     }
   }
 
-  _shuffle() {}
-
+  // Generate a deck array
   _buildDeck() {
     let deck = [];
     let values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
@@ -23,21 +21,41 @@ class App extends Component {
       })
     })
 
-    this.setState({ deck })
-    // this.shuffle();
+    // Deck is made, set it to state and shuffle it
+    this.setState({ deck }, () => this._shuffle());
   }
 
-  componentDidMount() {
+  // Fisher–Yates Shuffle
+  _shuffle() {
+    let deck = this.state.deck;
+    let currentIndex = deck.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = deck[currentIndex];
+      deck[currentIndex] = deck[randomIndex];
+      deck[randomIndex] = temporaryValue;
+    }
+    console.log(deck);
+    this.setState({ deck });
+  }
+
+  componentWillMount() {
     this._buildDeck();
   }
 
+
+  // Currently will render 3 random cards
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Blackjack Baby</h2>
         </div>
-        <img src={require(`../public/images/cards/${cards[1]}.png`)} role="presentation" />
+        <img src={require(`../public/images/cards/${this.state.deck[0]}.png`)} role="presentation" />
+        <img src={require(`../public/images/cards/${this.state.deck[1]}.png`)} role="presentation" />
+        <img src={require(`../public/images/cards/${this.state.deck[3]}.png`)} role="presentation" />
       </div>
     );
   }
