2f5b3f2f17d458f1faef36fdce4039a59a4b6161
Connor Garber <connor.garber@gmail.com>
2017-01-15 14:02:15 -0600

    decided on card rendering method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
+const cards = ['2C','QC'];
 
 class App extends Component {
   constructor() {
@@ -10,27 +10,13 @@ class App extends Component {
     }
   }
 
-  _getCards() {
-    axios.get('../public/images/cards/2C.png')
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });     
-    // this.setState({ cards });
-  }
-
-  componentDidMount() {
-    this._getCards();
-  }
-
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Blackjack Baby</h2>
         </div>
+        <img src={require(`../public/images/cards/${cards[1]}.png`)} role="presentation" />
       </div>
     );
   }
