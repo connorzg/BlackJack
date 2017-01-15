8c0241f166bc6a29faed1df0fb235da951992b6c
Connor Garber <connor.garber@gmail.com>
2017-01-15 14:32:23 -0600

    build card deck on startup

--- src/App.js
+++ src/App.js
@@ -6,8 +6,29 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      cards: []
+      deck: []
     }
+  }
+
+  _shuffle() {}
+
+  _buildDeck() {
+    let deck = [];
+    let values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+    let suits = ["C","D","H","S"];
+
+    values.forEach((value) => {
+      suits.forEach((suit) => {
+        deck.push(value + suit);
+      })
+    })
+
+    this.setState({ deck })
+    // this.shuffle();
+  }
+
+  componentDidMount() {
+    this._buildDeck();
   }
 
   render() {
