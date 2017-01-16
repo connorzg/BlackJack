b5f390bbdd5ba6be8edac0816985ad02965be97b
Connor Garber <connor.garber@gmail.com>
2017-01-15 18:56:13 -0600

    Added "hit" function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      deck: []
+      deck: [],
+      playerCount: 1,
+      player1: []
     }
   }
 
@@ -18,8 +20,8 @@ class App extends Component {
     values.forEach((value) => {
       suits.forEach((suit) => {
         deck.push(value + suit);
-      })
-    })
+      });
+    });
 
     // Deck is made, set it to state and shuffle it
     this.setState({ deck }, () => this._shuffle());
@@ -38,24 +40,50 @@ class App extends Component {
       deck[randomIndex] = temporaryValue;
     }
     console.log(deck);
-    this.setState({ deck });
+    let player1= [deck.pop(), deck.pop()];
+    this.setState({ deck, player1 }, () => console.log(this.state.player1));
   }
 
+  // "HIT ME"
+  _handleHit() {
+    let deck = this.state.deck;
+    let player1 = this.state.player1;
+    let newCard = deck.pop();
+    player1.push(newCard);
+    this.setState({player1, deck},() => {
+      console.log(this.state.player1, this.state.deck)
+    })
+  }
+
+  // Dynamically render the cards in a player's hand
+  _handlePlayerHand() {
+    let p1 = this.state.player1
+    let hand = [];
+    for (var i = 0; i < p1.length; i++) {
+      hand.push(<img key={i} src={require(`../public/images/cards/${p1[i]}.png`)} role="presentation" />)
+    }
+    return hand;
+  }
+
+  // build the shuffled deck on app start
   componentWillMount() {
     this._buildDeck();
   }
 
-
   // Currently will render 3 random cards
   render() {
+    let hand = this._handlePlayerHand()
+
     return (
       <div className="App">
         <div className="App-header">
           <h2>Blackjack Baby</h2>
         </div>
-        <img src={require(`../public/images/cards/${this.state.deck[0]}.png`)} role="presentation" />
-        <img src={require(`../public/images/cards/${this.state.deck[1]}.png`)} role="presentation" />
-        <img src={require(`../public/images/cards/${this.state.deck[3]}.png`)} role="presentation" />
+        <div className="Player1">
+          {hand}
+        </div>
+        <button onClick={() => this._handleHit()}>Hit</button>
+        <button>Stay</button>
       </div>
     );
   }
