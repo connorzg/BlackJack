e2d46ae98c3b47b85c9cc6d31d697d0ca29c2dac
Connor Garber <connor.garber@gmail.com>
2017-01-15 20:43:02 -0600

    get card value from file name, attempting to calculate hand value

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     this.state = {
       deck: [],
       playerCount: 1,
-      player1: []
+      player1: [],
+      score: 0
     }
   }
 
@@ -55,14 +56,40 @@ class App extends Component {
     })
   }
 
+  _handleStay() {
+    // TODO when math incorporated
+  }
+
   // Dynamically render the cards in a player's hand
-  _handlePlayerHand() {
+  _renderPlayerHand() {
     let p1 = this.state.player1
     let hand = [];
+    let score = this.state.score;
     for (var i = 0; i < p1.length; i++) {
-      hand.push(<img key={i} src={require(`../public/images/cards/${p1[i]}.png`)} role="presentation" />)
+      // the deck array matches the filename. Suits are irrelevent so value only needs first character.
+      let value = this._getCardValue(p1[i]);
+      score += value;
+      hand.push(<img key={i} value={value} src={require(`../public/images/cards/${p1[i]}.png`)} role="presentation" />)
     }
+    console.log(score);
     return hand;
+  }
+
+  _getCardValue(cardValue) {
+    if (cardValue[0] + cardValue[1] !== "10") {
+      cardValue = cardValue[0]
+    }
+
+    if (cardValue.match(/[A-Z]/)) {
+      // TODO Ace logic
+      return 10
+    } else {
+      return +cardValue
+    }
+  }
+
+  _calculateScore () {
+    // Need to figure out how to add card values and save to state
   }
 
   // build the shuffled deck on app start
@@ -72,18 +99,18 @@ class App extends Component {
 
   // Currently will render 3 random cards
   render() {
-    let hand = this._handlePlayerHand()
+    let hand = this._renderPlayerHand();
 
     return (
       <div className="App">
         <div className="App-header">
           <h2>Blackjack Baby</h2>
         </div>
-        <div className="Player1">
+        <div ref="p1" className="Player1">
           {hand}
         </div>
         <button onClick={() => this._handleHit()}>Hit</button>
-        <button>Stay</button>
+        <button onClick={() => this._handleStay()}>Stay</button>
       </div>
     );
   }
